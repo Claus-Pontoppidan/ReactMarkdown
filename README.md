b9c009965d715052fb0266371e569d626e8d06c1
Claus Pontoppidan <mr.cp@smartconnect.dk>
2022-06-24 22:24:41 +0200

    Initial commit
    Completed the tutorial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ReactMarkdown
+# react-markdown
+
+> Based on [Building a Markdown Blog With React by Cand Dev] (https://www.youtube.com/watch?v=Od-Uj5RSsuM)
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { LogoIcon, MoonIcon } from "./assets/icons";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="logo-wrapper">
+          <div className="logo">
+            <LogoIcon />
+            <span>Js Snippets</span>
+          </div>
+        
+        <button className="icon"><MoonIcon/></button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
